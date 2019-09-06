a0fe0510170a8f5b414c09d25d92f2421e1ca748
vishalicious213 <vishalicious213@gmail.com>
2019-09-06 13:25:38 -0400

    tabs - got topic array. need to get it into Tab function for naming assignment

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,14 +8,41 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+// 1. get data
+// 2. create html element
+// 3. do #2 for each piece of data?
+// 4. append to DOM
+
+// axios.get("https://lambda-times-backend.herokuapp.com/topics")
+//     .then(response => {
+//         console.log("Got data from Lambda");
+//         console.log(response);
+//         console.log(response.data.topics)
+//     })
+//     .catch(err => {
+//         console.log("Got error from Lambda", err)
+//     })
+
+
 axios.get("https://lambda-times-backend.herokuapp.com/topics")
     .then(response => {
         console.log("Got data from Lambda");
-        console.log(response)
-    })
+        console.log(response);
+        console.log(response.data.topics);
+        
+        const topicArr = response.data.topics;
+        console.log(topicArr);
+
+        topicArr.forEach(item => {
+            newItem = item;
+            console.log(newItem) // got array of topic names
+        }) // .topicArr
+    }) // .then
     .catch(err => {
         console.log("Got error from Lambda", err)
-    })
+    }) // .catch
+
+
 
 function Tab(arr) {
     // define new elements
@@ -30,7 +57,7 @@ function Tab(arr) {
     newTab.textContent = "topic here";
 
     arr.forEach(item => {
-        
+
     })
 
 }
